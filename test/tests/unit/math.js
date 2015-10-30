7ab7421d3efd0f82685bdd0a6fd60bc5e0b861b1
danqura <danqura@gmail.com>
2015-10-30 20:18:42 +0300

    add test for mult and div

--- test/tests/unit/math.js
+++ test/tests/unit/math.js
@@ -7,6 +7,13 @@ describe("Math module", function() {
 	it("#sub", function() {
 		expect(math.sub(3, 2)).to.equal(1);
 	});
+	it("#div", function() {
+		expect(math.div(4, 2)).to.equal(2);
+	});
+	it("#mult", function() {
+		expect(math.mult(4, 2)).to.equal(8);
+	});
+
 	it("#asyncSum", function(done) {
 		math.asyncSum(30000, 4000, function(result) {
 			expect(result).to.equal(34000);
