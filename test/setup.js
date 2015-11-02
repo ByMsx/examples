78310abf3c650db323af84b251bfaf41952b8070
Max <maxim-but@mail.ru>
2015-11-02 11:05:12 +0300

    Install muon-mockify

--- test/setup.js
+++ test/setup.js
@@ -2,3 +2,4 @@ global._	   = require("underscore");
 global.should  = require("should");
 global.expect  = require("expect.js");
 global.chai    = require("chai");
+global.mockify = require("muon-mockify");
